1b551c934960c2afbe12d6f2ec195a2ce1cb35ee
pmlaroche-agoralogie <philippe.laroche@agoralogie.fr>
2015-03-02 11:56:32 +0100

    Ajout config-cordova et dossier des

---
+++ www/cordova_plugins.js
@@ -0,0 +1,11 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+   
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.megster.cordova.rfduino": "0.0.2"
+}
+// BOTTOM OF METADATA
+});
